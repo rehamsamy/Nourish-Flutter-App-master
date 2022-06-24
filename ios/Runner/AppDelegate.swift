0a3bba56904abded38ea399efea9fa80c41768c8
Abdelrahman Mostafa Elmarakby <abdomostafa005@gmail.com>
2022-06-25 00:31:47 +0200

    maps ApiKey

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyCXFEuYNLDNZVkJN3SwCeMNYiIbc4AJDG8")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
